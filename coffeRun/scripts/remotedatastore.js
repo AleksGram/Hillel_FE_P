84b3d16eb46eaa9e9e2b8f39c97f6f2142dfa85e
AlexGram <agramatik1@gmail.com>
2018-01-23 23:56:27 +0200

    finished remoteDataStore

--- coffeRun/scripts/remotedatastore.js
+++ coffeRun/scripts/remotedatastore.js
@@ -14,6 +14,23 @@
             console.log(serverResponse);
         } )
     };
+    RemoteDataStore.prototype.getAll = function (cb) {
+        $.get(this.severUrl, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+    RemoteDataStore.prototype.get = function (key, cb) {
+        $.get(this.severUrl + '/' + key, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+    RemoteDataStore.prototype.remove = function (key) {
+        $.ajax(this.severUrl + '/' + key, {
+            type: 'DELETE'
+        });
+    };
     App.RemoteDataStore = RemoteDataStore;
     window.App = App;
 })(window);
