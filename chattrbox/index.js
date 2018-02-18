b48abf65244c778dc1edcc7267624e952928630b
AlexGram <agramatik1@gmail.com>
2018-02-18 23:40:24 +0200

    added error page and handle

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -1,9 +1,24 @@
 var http = require('http');
 var fs = require('fs');
+var extract = require('./extract');
+
+var handleError  = function (err, res) {
+    res.writeHead(302, {
+        'Location': 'error.html'
+    });
+    res.end();
+};
 var server = http.createServer(function (req, res) {
     console.log('Responding to a request.');
-    fs.readFile('app/index.html', function (error, data) {
-        res.end(data);
+    var filePath = extract(req.url);
+    fs.readFile(filePath, function (err, data) {
+        if(err){
+            handleError(err, res);
+            return;
+        } else {
+            res.end(data);
+
+        }
     })
 });
 server.listen(3000);
