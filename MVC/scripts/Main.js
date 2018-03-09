f723911a8db7c046c0b09ce17ac057fd8648563a
AlexGram <agramatik1@gmail.com>
2018-03-10 01:27:07 +0200

    mvc

--- MVC/scripts/Main.js
+++ MVC/scripts/Main.js
@@ -8,22 +8,66 @@ var listContainer = $('.itemsListContainer');
 
 function TasksList() {
     this.el = itemsList;
+    for(var i = 0; i < localStorage.length; i++){
+        this.push(new Task(JSON.parse(localStorage[i]).text));
+    }
+    addButton.click(this.addTask);
+    deleteButton.click(this.deleteIfDone);
+    hideShowButton.click(this.hideDone);
+
 }
 TasksList.prototype = [];
 var listProto = TasksList.prototype;
 
-listProto.push = function (item) {
+listProto.push = function (text) {
+    var item = new Task(text);
     [].push.call(this, item);
-    itemsList.append(item.render());
+    var id = this.length - 1;
+    this.el.append(item.render());
+    item.id = id;
+    item.save();
 };
-listProto.deleteDone = function () {
- $.each(taskList, function (index, value) {
-      if(value.isDone){
-          value.el.remove();
-          taskList.splice(index, 1);
-      }
-  })
+listProto.deleteIfDone = function () {
+     $.each(taskList, function (index, value) {
+         if(taskList[index]){
+             if(value.isDone){
+                 value.el.remove();
+                 taskList.splice(index, 1);
+             }
+         }
+      });
 };
+listProto.addTask = function () {
+    var text = inputField.val();
+    if (text === ""){
+        taskList.emptyFieldError();
+    } else {
+        // var task = new Task(text);
+        taskList.push(text);
+        taskList.clearAndFocusInput();
+    }
+};
+listProto.clearAndFocusInput = function () {
+    $(inputField).val("");
+    $(inputField).focus();
+};
+listProto.emptyFieldError = function() {
+    var errorText  = document.getElementById('error');
+    errorText.classList.add('showError');
+    setTimeout(function () {
+        errorText.classList.remove('showError');
+    },500);
+};
+listProto.hideDone = function() {
+    listContainer.toggleClass('hide');
+    if (listContainer.hasClass('hide')) {
+        hideShowButton.text("show completed");
+    } else {
+        hideShowButton.text("hide completed");
+    }
+};
+
+
 var taskList = new TasksList();
 
 function Task(text) {
@@ -32,7 +76,7 @@ function Task(text) {
 };
 var proto = Task.prototype;
 
-proto.render = function () {
+Task.prototype.render = function () {
     this.el = document.createElement('li');
     $(this.el).text(this.text);
     $(this.el).click(this.done.bind(this));
@@ -47,8 +91,9 @@ proto.addEditButton = function (task) {
 };
 proto.done = function (e) {
     if(e.target.tagName === 'LI'){
-        this.isDone = true;
+        this.isDone = !this.isDone;
         $(this.el).toggleClass('done');
+        this.save();
     }
 };
 proto.editTask = function (e) {
@@ -66,29 +111,9 @@ proto.editTask = function (e) {
         }
     }
 };
-proto.deleteTask = function () {
+proto.save = function () {
+    localStorage[this.id] = JSON.stringify(this);
+};
 
-};
-addButton.click(function () {
-    var text = inputField.val();
-    if (text === ""){
-        emptyFieldError();
-    } else {
-        var task = new Task(text);
-        taskList.push(task);
-        clearAndFocusInput();
-    }
-});
-// deleteButton.click(deleteItem);
-var clearAndFocusInput = function () {
-    $(inputField).val("");
-    $(inputField).focus();
-};
-function emptyFieldError() {
-    var errorText  = document.getElementById('error');
-    errorText.classList.add('showError');
-    setTimeout(function () {
-        errorText.classList.remove('showError');
-    },500);
-};
+
 
