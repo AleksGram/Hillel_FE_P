7c3fb32d6ad11b71c78f7bb0a0a3589aae8b976a
AlexGram <agramatik1@gmail.com>
2018-03-11 22:10:23 +0200

    mvc2 finish

--- MVC/scripts/Main.js
+++ MVC/scripts/Main.js
@@ -4,71 +4,8 @@ var hideShowButton = $("#hideButton");
 var inputField = $("#inputValue");
 var itemsList = $("#itemsList");
 var listContainer = $('.itemsListContainer');
+var myApp = [];
 
-
-function TasksList() {
-    this.el = itemsList;
-    for(var i = 0; i < localStorage.length; i++){
-        this.push(new Task(JSON.parse(localStorage[i]).text));
-    }
-    addButton.click(this.addTask);
-    deleteButton.click(this.deleteIfDone);
-    hideShowButton.click(this.hideDone);
-
-}
-TasksList.prototype = [];
-var listProto = TasksList.prototype;
-
-listProto.push = function (text) {
-    var item = new Task(text);
-    [].push.call(this, item);
-    var id = this.length - 1;
-    this.el.append(item.render());
-    item.id = id;
-    item.save();
-};
-listProto.deleteIfDone = function () {
-     $.each(taskList, function (index, value) {
-         if(taskList[index]){
-             if(value.isDone){
-                 value.el.remove();
-                 taskList.splice(index, 1);
-             }
-         }
-      });
-};
-listProto.addTask = function () {
-    var text = inputField.val();
-    if (text === ""){
-        taskList.emptyFieldError();
-    } else {
-        // var task = new Task(text);
-        taskList.push(text);
-        taskList.clearAndFocusInput();
-    }
-};
-listProto.clearAndFocusInput = function () {
-    $(inputField).val("");
-    $(inputField).focus();
-};
-listProto.emptyFieldError = function() {
-    var errorText  = document.getElementById('error');
-    errorText.classList.add('showError');
-    setTimeout(function () {
-        errorText.classList.remove('showError');
-    },500);
-};
-listProto.hideDone = function() {
-    listContainer.toggleClass('hide');
-    if (listContainer.hasClass('hide')) {
-        hideShowButton.text("show completed");
-    } else {
-        hideShowButton.text("hide completed");
-    }
-};
-
-
-var taskList = new TasksList();
 
 function Task(text) {
     this.text = text;
@@ -90,16 +27,16 @@ proto.addEditButton = function (task) {
     $(task).append(editButton);
 };
 proto.done = function (e) {
-    if(e.target.tagName === 'LI'){
+    if (e.target.tagName === 'LI') {
         this.isDone = !this.isDone;
         $(this.el).toggleClass('done');
         this.save();
     }
 };
 proto.editTask = function (e) {
-    if (e.target.tagName === 'BUTTON'){
-        if(e.target.classList.contains('save')){
-            e.target.parentNode.innerHTML = inputField.val() + '<button>Edit</button>' ;
+    if (e.target.tagName === 'BUTTON') {
+        if (e.target.classList.contains('save')) {
+            e.target.parentNode.innerHTML = inputField.val() + '<button>Edit</button>';
             e.target.classList.remove('save');
             inputField.val("");
         } else {
@@ -112,8 +49,88 @@ proto.editTask = function (e) {
     }
 };
 proto.save = function () {
-    localStorage[this.id] = JSON.stringify(this);
+    myApp[this.id] = (this);
+    localStorage.setItem('myApp', JSON.stringify(myApp));
 };
+
+function TasksList() {
+    this.el = itemsList;
+    var ls = JSON.parse(localStorage.getItem('myApp'));
+    if (ls) {
+        for (var i = 0; i < ls.length; i++) {
+            this.push(ls[i].text);
+        }
+    }
+    addButton.click(this.addTask);
+    deleteButton.click(this.deleteIfDone);
+    hideShowButton.click(this.hideDone);
+
+}
+
+TasksList.prototype = [];
+var listProto = TasksList.prototype;
+
+listProto.push = function (text) {
+    var item = new Task(text);
+    [].push.call(this, item);
+    var id = this.length - 1;
+    this.el.append(item.render());
+    item.id = id;
+    item.save();
+};
+listProto.deleteIfDone = function () {
+    for (var i = 0; i < taskList.length; i++){
+        if(taskList[i]){
+            if(taskList[i].isDone){
+                taskList[i].el.remove();
+                taskList.updateStorage(taskList[i].id);
+                taskList.splice(i, 1);
+                i--;
+            }
+        }
+    }
+};
+listProto.updateStorage = function (id) {
+    for(var i =0; i < myApp.length; i++){
+        if(myApp[i]){
+            if(id === myApp[i].id){
+                myApp.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    localStorage.setItem('myApp', JSON.stringify(myApp));
+};
+listProto.addTask = function () {
+    var text = inputField.val();
+    if (text === "") {
+        taskList.emptyFieldError();
+    } else {
+        taskList.push(text);
+        taskList.clearAndFocusInput();
+    }
+};
+listProto.clearAndFocusInput = function () {
+    $(inputField).val("");
+    $(inputField).focus();
+};
+listProto.emptyFieldError = function () {
+    var errorText = document.getElementById('error');
+    errorText.classList.add('showError');
+    setTimeout(function () {
+        errorText.classList.remove('showError');
+    }, 500);
+};
+listProto.hideDone = function () {
+    listContainer.toggleClass('hide');
+    if (listContainer.hasClass('hide')) {
+        hideShowButton.text("show completed");
+    } else {
+        hideShowButton.text("hide completed");
+    }
+};
+var taskList = new TasksList();
+
 
 
 
